527186a3bf7a299811d60bfb65eb3183ab3e4f90
alexandra26arkhipenko <32879488+alexandra26arkhipenko@users.noreply.github.com>
2017-11-19 13:11:46 +0300

    BinarySearchTree

--- NET.W.2017.Arkhipenko.13.02/MatrixSquare.Test/MatrixSquareTest.cs
+++ NET.W.2017.Arkhipenko.13.02/MatrixSquare.Test/MatrixSquareTest.cs
@@ -10,26 +10,30 @@ namespace MatrixSquare.Test
         public void MatrixSqureClassicTest()
         {
             var matrixSquare = new MatrixSquare<int>(3);
-            var mt = new MatrixSymmetrical<int>(3);
+            var mt = new MatrixSymmetrical<int>(3)
+            {
+                [0, 0] = 1,
+                [0, 1] = 2,
+                [0, 2] = 3,
+                [1, 1] = 4,
+                [1, 2] = 5,
+                [2, 2] = 6
+            };
 
-            mt[0, 0] = 1;
-            mt[0, 1] = 2;
-            mt[0, 2] = 3;
-
-            mt[1, 1] = 4;
-            mt[1, 2] = 5;
-            mt[2, 2] = 6;
 
 
-            var mt1 = new MatrixSquareDiagonal<int>(3);
 
-            mt1[0, 0] = 1;
-            mt1[0, 1] = 2;
-            mt1[0, 2] = 3;
+            var mt1 = new MatrixSquareDiagonal<int>(3)
+            {
+                [0, 0] = 1,
+                [0, 1] = 2,
+                [0, 2] = 3,
+                [1, 1] = 4,
+                [1, 2] = 5,
+                [2, 2] = 6
+            };
 
-            mt1[1, 1] = 4;
-            mt1[1, 2] = 5;
-            mt1[2, 2] = 6;
+
 
 
             _log = false;
