a1820f4112a4c683e7044bf04103dd8e55fdb22e
alexandra26arkhipenko <32879488+alexandra26arkhipenko@users.noreply.github.com>
2017-11-25 13:45:55 +0300

    Correct mistakes

--- NET.W.2017.Arkhipenko.13.03/Tree.Test/BinarySearchTreeTest.cs
+++ NET.W.2017.Arkhipenko.13.03/Tree.Test/BinarySearchTreeTest.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Tree;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tree.Test
 {
@@ -12,20 +9,20 @@ namespace Tree.Test
         public void AddItemInt()
         {
             var tree = new BinarySearchTree<int> {1, 5, 3, 7, 4};
-            Assert.AreEqual(tree.Count, 5); 
+            Assert.AreEqual(tree.Count, 5);
         }
 
         [TestMethod]
         public void FindItemInt()
         {
-            var tree = new BinarySearchTree<int> { 1, 5, 3, 7, 4 };
+            var tree = new BinarySearchTree<int> {1, 5, 3, 7, 4};
             Assert.AreEqual(tree.Find(5), 5);
         }
 
         [TestMethod]
         public void RemoveItemInt()
         {
-            var tree = new BinarySearchTree<int> { 1, 5, 3, 7, 4 };
+            var tree = new BinarySearchTree<int> {1, 5, 3, 7, 4};
             Assert.AreEqual(tree.Count, 5);
             tree.Remove(4);
             Assert.AreEqual(tree.Count, 4);
@@ -34,21 +31,21 @@ namespace Tree.Test
         [TestMethod]
         public void AddItemString()
         {
-            var tree = new BinarySearchTree<string> { "a", "b", "c", "d" };
+            var tree = new BinarySearchTree<string> {"a", "b", "c", "d"};
             Assert.AreEqual(tree.Count, 4);
         }
 
         [TestMethod]
         public void FindItemString()
         {
-            var tree = new BinarySearchTree<string> { "a", "b", "c", "d" };
+            var tree = new BinarySearchTree<string> {"a", "b", "c", "d"};
             Assert.AreEqual(tree.Find("b"), "b");
         }
 
         [TestMethod]
         public void RemoveItemString()
         {
-            var tree = new BinarySearchTree<string> { "a", "b", "c", "d" };
+            var tree = new BinarySearchTree<string> {"a", "b", "c", "d"};
             Assert.AreEqual(tree.Count, 4);
             tree.Remove("a");
             Assert.AreEqual(tree.Count, 3);
@@ -57,23 +54,23 @@ namespace Tree.Test
         [TestMethod]
         public void AddItemBook()
         {
-            var tree = new BinarySearchTree<Book> { new Book("qaz"), new Book("wsx"), new Book("edc")};
+            var tree = new BinarySearchTree<Book> {new Book("qaz"), new Book("wsx"), new Book("edc")};
             Assert.AreEqual(tree.Count, 3);
         }
 
         [TestMethod]
         public void FindItemBook()
         {
-            Book book = new Book("qaz");
-            var tree = new BinarySearchTree<Book> { book, new Book("wsx"), new Book("edc") };
+            var book = new Book("qaz");
+            var tree = new BinarySearchTree<Book> {book, new Book("wsx"), new Book("edc")};
             Assert.AreEqual(tree.Find(book), book);
         }
 
         [TestMethod]
         public void RemoveItemBook()
         {
-            Book book = new Book("qaz");
-            var tree = new BinarySearchTree<Book> { book, new Book("wsx"), new Book("edc") };
+            var book = new Book("qaz");
+            var tree = new BinarySearchTree<Book> {book, new Book("wsx"), new Book("edc")};
             Assert.AreEqual(tree.Count, 3);
             tree.Remove(book);
             Assert.AreEqual(tree.Count, 2);
@@ -89,7 +86,7 @@ namespace Tree.Test
                 return -1;
             }
 
-            var tree = new BinarySearchTree<Point> (Comparer){ new Point(1,2), new Point(3, 4), new Point(5, 2) };
+            var tree = new BinarySearchTree<Point>(Comparer) {new Point(1, 2), new Point(3, 4), new Point(5, 2)};
             Assert.AreEqual(tree.Count, 3);
         }
 
@@ -102,8 +99,9 @@ namespace Tree.Test
                 if (p1.X == p2.X) return 0;
                 return -1;
             }
-            Point point = new Point(1, 2);
-            var tree = new BinarySearchTree<Point>(Comparer) {point , new Point(3, 4), new Point(5, 2) };
+
+            var point = new Point(1, 2);
+            var tree = new BinarySearchTree<Point>(Comparer) {point, new Point(3, 4), new Point(5, 2)};
             Assert.AreEqual(tree.Find(point), point);
         }
 
@@ -116,8 +114,9 @@ namespace Tree.Test
                 if (p1.X == p2.X) return 0;
                 return -1;
             }
-            Point point = new Point(1, 2);
-            var tree = new BinarySearchTree<Point>(Comparer) { point, new Point(3, 4), new Point(5, 2) };
+
+            var point = new Point(1, 2);
+            var tree = new BinarySearchTree<Point>(Comparer) {point, new Point(3, 4), new Point(5, 2)};
             Assert.AreEqual(tree.Count, 3);
             tree.Remove(point);
             Assert.AreEqual(tree.Count, 2);
--- NET.W.2017.Arkhipenko.13.03/Tree/BinarySearchTree.cs
+++ NET.W.2017.Arkhipenko.13.03/Tree/BinarySearchTree.cs
@@ -1,53 +1,72 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using static Tree.BinarySearchTreeHelper;
 
 namespace Tree
 {
-    public class BinarySearchTree<T> :  IEnumerable<T>, IEnumerable
+    public class BinarySearchTree<T> : IEnumerable<T>, IEnumerable
     {
-        
-
         #region private fields
-        private Tree<T> _tree;
-        private int _count;
-        private Comparison<T> Comparer { get;}
+
+        private Node<T> _tree;
+        private Comparison<T> Comparer { get; }
+
         #endregion
 
         #region ctor
+
         public BinarySearchTree()
         {
-            Comparer = (lhs, rhs) => GetComparer<T>().Compare(lhs, rhs);
+            Comparer = (lhs, rhs) => GetComparer().Compare(lhs, rhs);
         }
+
         public BinarySearchTree(Comparison<T> comparer)
         {
             Comparer = comparer;
         }
+
         #endregion
 
         #region public
 
+        public static IComparer<T> GetComparer()
+        {
+            var type = typeof(T);
+
+            if (type == typeof(string))
+                return StringComparer.CurrentCulture as IComparer<T>;
+
+            if (!ReferenceEquals(type.GetInterface("IComparable`1"), null) ||
+                !ReferenceEquals(type.GetInterface("IComparable"), null))
+                return Comparer<T>.Default;
+
+            throw new ArgumentException();
+        }
+
         #region functions of the tree
+
         /// <summary>
-        /// Add element to the tree
+        ///     Add element to the tree
         /// </summary>
         /// <param name="item"> the element which is added </param>
         public void Add(T item)
         {
-            BinarySearchTreeHelper.Add(ref _tree, item, Comparer);
-            _count++;
+            Add(ref _tree, item, Comparer);
+            Count++;
         }
 
         /// <summary>
-        /// Find element into the tree
+        ///     Find element into the tree
         /// </summary>
         /// <param name="item"> the element which we are looking for</param>
         /// <returns>found item</returns>
-        public T Find(T item) => BinarySearchTreeHelper.Find(ref _tree, item, Comparer);
+        public T Find(T item)
+        {
+            return Find(ref _tree, item, Comparer);
+        }
 
         /// <summary>
-        /// Remove element from the tree
+        ///     Remove element from the tree
         /// </summary>
         /// <param name="item">the element that is removig</param>
         /// <returns></returns>
@@ -59,82 +78,311 @@ namespace Tree
         public bool Remove(T item, Comparison<T> comparer)
         {
             if (ReferenceEquals(item, null))
-            {
                 throw new ArgumentNullException(nameof(item));
-            }
 
             if (ReferenceEquals(comparer, null))
-            {
                 throw new ArgumentNullException(nameof(comparer));
-            }
 
             var temp = _tree;
-            var removed = BinarySearchTreeHelper.Remove(ref _tree, ref temp, item, comparer);
+            var removed = Remove(ref _tree, ref temp, item, comparer);
             if (removed)
-            {
-                _count--;
-            }
+                Count--;
 
             return removed;
         }
 
         /// <summary>
-        /// Amount od the elements into the tree
+        ///     Amount od the elements into the tree
         /// </summary>
-        public int Count => _count;
+        public int Count { get; private set; }
 
         #endregion
 
+
         #region IEnumerable
+
         /// <summary>
-        /// IEnumerable implementation
+        ///     IEnumerable implementation
         /// </summary>
         /// <returns></returns>
-        public IEnumerator<T> GetEnumerator() => GetInorderEnumerator().GetEnumerator();
-        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        public IEnumerator<T> GetEnumerator()
+        {
+            return GetInorderEnumerator().GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
 
 
         public IEnumerable<T> GetPreorderEnumerator()
         {
             if (ReferenceEquals(_tree, null))
-            {
                 yield break;
-            }
 
             foreach (var item in GetPrefixEnumerator(_tree))
-            {
                 yield return item;
-            }
         }
 
-       
+
         public IEnumerable<T> GetInorderEnumerator()
         {
             if (ReferenceEquals(_tree, null))
-            {
                 yield break;
-            }
 
             foreach (var item in GetInfixEnumerator(_tree))
-            {
                 yield return item;
-            }
         }
 
-      
+
         public IEnumerable<T> GetPostorderEnumerator()
         {
             if (ReferenceEquals(_tree, null))
-            {
                 yield break;
-            }
 
             foreach (var item in GetPostfixEnumerator(_tree))
-            {
                 yield return item;
+        }
+
+        #endregion
+
+        #endregion
+
+        #region private
+
+        /// <summary>
+        ///     Add element to the tree
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="tree"> the tree where the element is added</param>
+        /// <param name="item"> the element which is added</param>
+        /// <param name="comparer">comparator</param>
+        private static void Add(ref Node<T> tree, T item, Comparison<T> comparer)
+        {
+            if (ReferenceEquals(tree, null))
+            {
+                tree = new Node<T>(null, null, item);
+                return;
+            }
+
+            var resultComparer = comparer(tree.Data, item);
+            if (resultComparer == 0)
+            {
+                tree.Data = item;
+                return;
+            }
+
+            if (resultComparer > 0)
+            {
+                Add(ref tree.Left, item, comparer);
+                return;
+            }
+
+            Add(ref tree.Right, item, comparer);
+        }
+
+        /// <summary>
+        ///     Find element into the tree
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="tree"> the tree, where we looking for an element </param>
+        /// <param name="item"> the element which we looking for</param>
+        /// <param name="comparer">comparator</param>
+        private static T Find(ref Node<T> tree, T item, Comparison<T> comparer)
+        {
+            while (true)
+            {
+                if (ReferenceEquals(tree, null))
+                    return default(T);
+
+                var result = comparer(tree.Data, item);
+                if (result == 0)
+                    return tree.Data;
+
+                if (result > 0)
+                {
+                    tree = tree.Left;
+                    continue;
+                }
+
+                tree = tree.Right;
             }
         }
+
+        /// <summary>
+        ///     Remove element from the tree
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="tree"> child tree </param>
+        /// <param name="treeGtreater"> parent tree </param>
+        /// <param name="item">the element that is removing</param>
+        /// <param name="comparer">comparator</param>
+        /// <returns></returns>
+        private static bool Remove(ref Node<T> tree, ref Node<T> treeGtreater, T item, Comparison<T> comparer)
+        {
+            if (ReferenceEquals(tree, null))
+                return false;
+            var result = comparer(item, tree.Data);
+            if (result < 0)
+            {
+                treeGtreater = tree;
+                return Remove(ref tree.Left, ref treeGtreater, item, comparer);
+            }
+
+            if (result > 0)
+            {
+                treeGtreater = tree;
+                return Remove(ref tree.Right, ref treeGtreater, item, comparer);
+            }
+
+            if (ReferenceEquals(tree.Left, null) && ReferenceEquals(tree.Right, null))
+            {
+                tree = null;
+                return true;
+            }
+
+            if (!ReferenceEquals(tree.Right, null) && ReferenceEquals(tree.Left, null))
+            {
+                tree = tree.Right;
+                return true;
+            }
+
+            if (ReferenceEquals(tree.Right, null))
+            {
+                tree = tree.Left;
+                return true;
+            }
+
+            if (ReferenceEquals(tree.Right.Left, null))
+            {
+                tree.Data = tree.Right.Data;
+                tree.Right = tree.Right.Right;
+                return true;
+            }
+
+            while (!ReferenceEquals(tree.Right.Left, null))
+            {
+                treeGtreater = tree.Right;
+                tree.Right = tree.Right.Left;
+            }
+
+            Remove(ref tree, ref treeGtreater, tree.Right.Data, comparer);
+            tree.Data = tree.Right.Data;
+            return true;
+        }
+
+        #region TRAVERSE
+
+        /// <summary>
+        ///     PREFIX_TRAVERSE
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="tree"></param>
+        /// <returns></returns>
+        private static IEnumerable<T> GetPrefixEnumerator(Node<T> tree)
+        {
+            while (true)
+            {
+                yield return tree.Data;
+
+                if (!ReferenceEquals(tree.Left, null))
+                    foreach (var item in GetPrefixEnumerator(tree.Left))
+                        yield return item;
+
+                if (!ReferenceEquals(tree.Right, null))
+                {
+                    tree = tree.Right;
+                    continue;
+                }
+
+                break;
+            }
+        }
+
+        /// <summary>
+        ///     INFIX_TRAVERSE
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="tree"></param>
+        /// <returns></returns>
+        private static IEnumerable<T> GetInfixEnumerator(Node<T> tree)
+        {
+            while (true)
+            {
+                if (!ReferenceEquals(tree.Left, null))
+                    foreach (var item in GetInfixEnumerator(tree.Left))
+                        yield return item;
+
+                yield return tree.Data;
+
+                if (!ReferenceEquals(tree.Right, null))
+                {
+                    tree = tree.Right;
+                    continue;
+                }
+
+                break;
+            }
+        }
+
+        /// <summary>
+        ///     POSTFIX_TRAVERSE
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        private static IEnumerable<T> GetPostfixEnumerator(Node<T> tree)
+        {
+            if (!ReferenceEquals(tree.Left, null))
+                foreach (var item in GetPostfixEnumerator(tree.Left))
+                    yield return item;
+
+            if (!ReferenceEquals(tree.Right, null))
+                foreach (var item in GetPostfixEnumerator(tree.Right))
+                    yield return item;
+
+            yield return tree.Data;
+        }
+
         #endregion
-#endregion
+
+        #endregion
+    }
+
+    /// <summary>
+    ///     The tree
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    internal class Node<T>
+    {
+        #region ctor
+
+        /// <summary>
+        ///     Ctor
+        /// </summary>
+        /// <param name="left">the left subtree </param>
+        /// <param name="right">the right subtree</param>
+        /// <param name="data">tree top</param>
+        public Node(Node<T> left, Node<T> right, T data)
+        {
+            Left = left;
+            Right = right;
+            Data = data;
+        }
+
+        #endregion
+
+        #region fields
+
+        /// <summary>
+        ///     Left - is the left subtree, Right - is the right subtree, Data - tree top
+        /// </summary>
+        internal Node<T> Left;
+
+        internal Node<T> Right;
+        internal T Data { get; set; }
+
+        #endregion
     }
 }
